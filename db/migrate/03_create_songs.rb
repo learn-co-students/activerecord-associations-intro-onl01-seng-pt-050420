f32db8858953be313f7f61ac35cbf9aa0c0550dc
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-07-13 17:36:44 +0000

    Done.

--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -1,2 +1,11 @@
 class CreateSongs < ActiveRecord::Migration[4.2]
+  
+  def change
+    create_table :songs do |column|
+      column.string :name 
+      column.integer :artist_id
+      column.integer :genre_id
+    end
+  end
+  
 end
